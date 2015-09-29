31c3346a138b92ac19e166365e2c0e465e775e0b
YouXianMing <705786299@qq.com>
2015-09-29 20:08:03 +0800

    Initial Commit

---
+++ SingletonPattern/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SingletonPattern
+//
+//  Created by YouXianMing on 15/9/29.
+//  Copyright © 2015年 YouXianMing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
